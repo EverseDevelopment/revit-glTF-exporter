db5076b85dfa93b816c895c27d5665ff6181f17c
vnoves <vnoves@e-verse.com>
2024-06-10 16:00:29 -0300

    Adjusted WixVersion
    Allow downgrade
    
    RollBack Manufacturer
    
    Test Debug
    
    Adjusted action
    
    Adjusted Wixtoolset version
    
    Adjust uninstall
    
    Remove Verification
    
    Adjusted waiting time

--- GltfInstaller/Program.cs
+++ GltfInstaller/Program.cs
@@ -68,7 +68,7 @@ namespace GltfInstaller
             project.ManagedUI = new ManagedUI();
 
             project.ControlPanelInfo.ProductIcon = "Resources\\logo.ico";
-
+            
             project.ManagedUI.InstallDialogs.Add<GltfInstaller.WelcomeDialog>()
                                             .Add<GltfInstaller.LicenceDialog>()
                                             .Add<GltfInstaller.ProgressDialog>()
@@ -144,12 +144,13 @@ namespace GltfInstaller
             catch (Exception ex)
             {
                 session.Log("Error checking Revit process: " + ex.Message);
+                MessageBox.Show("Error", ex.Message);
                 return ActionResult.Failure;
             }
             return ActionResult.Success;
         }
 
-        public static bool WaitForFilesToBeOverwritable(string filePath, int waitTimeMilliseconds = 1000, int maxAttempts = 10)
+        public static bool WaitForFilesToBeOverwritable(string filePath, int waitTimeMilliseconds = 1000, int maxAttempts = 15)
         {
                 int attempts = 0;
                 while (IsFileInUse(filePath) && attempts < maxAttempts)
